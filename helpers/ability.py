798cd1658d78e903e0d32218b31a686ba625a67d
Luichang <charlesbarbret@yahoo.com>
2022-04-18 19:37:36 +0200

    renamed spells list to feat

--- helpers/ability.py
+++ helpers/ability.py
@@ -3,14 +3,14 @@ from starfinder_feats.starfinder_feat_type import FeatType
 class Ability:
     """The ability class for Starfinder Classes
     """
-    def __init__(self, name : str, level : int, _type : FeatType, *, short : str = None, 
-                 skills : list[(str, int)] = None, spells : list = None) -> None:
+    def __init__(self, name : str, level : int, _type : FeatType, *, short : str = None,
+                 skills : list[(str, int)] = None, feat : list = None) -> None:
         self.name = name
         self.level = level
         self.type = _type
         self.short = short
         self.skills = skills # an option can be any, needs to be remembered (at least in races)
-        self.spells = spells
+        self.feat = feat
     def __str__(self) -> str:
         return self.name
 
